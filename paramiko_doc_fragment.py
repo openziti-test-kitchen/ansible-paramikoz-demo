f10229e34d75f71f8563e2282bc80ef17a20f31d
Steven A. Broderick Elias <steven.broderick@netfoundry.io>
2022-06-23 22:30:11 -0400

    Fixing ansible-doc integration and varible precedence. Reduce code footprint

---
+++ paramiko_doc_fragment.py
@@ -0,0 +1,9 @@
+"""TODO"""
+from ansible.plugins.connection.paramiko_ssh import \
+    DOCUMENTATION as PARAMIKO_DOCUMENTATION
+
+
+class ModuleDocFragment():
+    "Base Paramiko Documentation"
+    # pylint: disable=too-few-public-methods
+    DOCUMENTATION = PARAMIKO_DOCUMENTATION
--- paramikoz.py
+++ paramikoz.py
@@ -1,48 +1,39 @@
 """OpenZiti paramiko connection plugin wrapper"""
 import os
 
-import yaml
-from ansible.plugins.connection.paramiko_ssh import (
-    DOCUMENTATION as PARAMIKO_SSH_DOC,
-    Connection as ParamikoConnection
-)
-
 import openziti
+from ansible.plugins.connection.paramiko_ssh import \
+    Connection as ParamikoConnection
 
-plugin_doc = yaml.full_load(PARAMIKO_SSH_DOC)
+DOCUMENTATION = '''
+    extends_documentation_fragment: paramiko_doc_fragment
+    options:
+      ziti_identities:
+        description: ziti identities to use for connection
+        default: null
+        env:
+          - name: ANSIBLE_ZITI_IDENTITIES
+        ini:
+          - section: paramikoz_connection
+            key: ziti_identities
+        vars:
+          - name: ziti_identities
+        required: true
+        type: pathlist
 
-EXT_MSG = 'Extended by OpenZiti.'
-
-EXT_OPTS = {
-    "ziti_identities": {
-        "description": "ziti identities to use for connection",
-        "default": None,
-        "env": [{"name": "ANSIBLE_ZITI_IDENTITIES"}],
-        "ini": [{"section": "paramikoz_connection", "key": "ziti_identities"}],
-        "var": [{"name": "ziti_identities"}],
-        "required": True,
-        "type": "pathlist",
-        "version_added": "1.0'"
-    },
-    "ziti_log_level": {
-        "description": "verbosity of ziti library",
-        "default": 0,
-        "env": [{"name": "ANSIBLE_ZITI_LOG_LEVEL"}],
-        "ini": [{"section": "paramikoz_connection", "key": "ziti_log_level"}],
-        "var": [{"name": "ziti_log"}],
-        "required": False,
-        "type": "integer",
-        "version_added": "1.0"
-    }
-}
-
-plugin_doc['author'] += f" - {EXT_MSG}"
-plugin_doc['name'] = 'paramikoz'
-plugin_doc['short_description'] += f". {EXT_MSG}"
-plugin_doc['description'].append(EXT_MSG)
-plugin_doc['options'] = {**plugin_doc['options'], **EXT_OPTS}
-
-DOCUMENTATION = yaml.dump(plugin_doc)
+      ziti_log_level:
+        description: verbosity of ziti library
+        default: 0
+        env:
+          - name: ANSIBLE_ZITI_LOG_LEVEL
+        ini:
+          - section: paramikoz_connection
+            key: ziti_log_level
+        vars:
+          - name: ziti_log_level
+        required: false
+        type: integer
+'''
 
 
 class Connection(ParamikoConnection):
@@ -51,18 +42,15 @@ class Connection(ParamikoConnection):
 
     transport = 'paramikoz'
 
-    def __init__(self, *args, **kwargs):
-        """init wrapper"""
-        super().__init__(*args, **kwargs)
+    def _connect(self):
+        '''Wrap connection activation object with ziti'''
+
         self.log_level = self.get_option('ziti_log_level')
         if os.getenv('ZITI_LOG') is None:
             os.environ['ZITI_LOG'] = str(self.log_level)
         self.identities = self.get_option('ziti_identities')
         for identity in self.identities:
             openziti.load(identity)
-
-    def _connect(self):
-        '''Wrap connection activation object with ziti'''
 
         with openziti.monkeypatch():
             super()._connect()
